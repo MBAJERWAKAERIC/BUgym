1273a56a1f9babab5aae8ed8f85187d5278b0117
MBAJERWAKAERIC <erickmbanjerwaka@gmail.com>
2022-10-27 19:33:18 +0300

    about

---
+++ admin/about - Copy.php
@@ -0,0 +1,65 @@
+<?php include('../constant/layout/head.php');?>
+<?php include('../constant/layout/header.php');?>
+<?php include('../constant/layout/sidebar.php');
+?>
+ 
+        <div class="page-wrapper">
+            
+            <div class="row page-titles">
+                <div class="col-md-5 align-self-center">
+                    <h3 class="text-primary">Add Brand Management</h3> </div>
+                <div class="col-md-7 align-self-center">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+                        <li class="breadcrumb-item active">Add Brand</li>
+                    </ol>
+                </div>
+            </div>
+            
+            
+            <div class="container-fluid">
+                <!--  Author Name: MayuriK. 
+ for any PHP, Codeignitor or Laravel work visit www.mayurik.com  -->
+                
+                
+                
+                <div class="row">
+                    <div class="col-lg-8" style="    margin-left: 10%;">
+                        <section class="card">
+<div class="card-header user-header alt bg-dark">
+<div class="media">
+<a href="#">
+<img class="align-self-center rounded-circle mr-3" style="width:85px; height:85px;" alt="" src="assets/uploadImage/Logo/favicon.png">
+</a>
+<div class="media-body">
+<h2 class="text-light display-6">Author Name : Mayuri K</h2>
+<p style="color: white">Freelancer - PHP, Codeignitor & Laravel</p>
+</div>
+</div>
+</div>
+<ul class="list-group list-group-flush">
+<li class="list-group-item">
+<a href="#" style="font-size:20px;" > <i class="fa fa-envelope-o"></i> Email <span class="badge badge-primary pull-right m-l-10 m-b-10" style="font-size:20px;" >mayuri.infospace@gmail.com</span></a>
+</li>
+<li class="list-group-item">
+<a style="font-size:20px;" href="https://mayurik.com" target="_blank" > <i class="fa fa-tasks"></i> My Previous Work <span class="badge badge-danger pull-right m-l-10 m-b-10" style="font-size:20px;" >www.mayurik.com</span></a>
+</li>
+<li class="list-group-item">
+<a style="font-size:20px;" href="https://www.youtube.com/c/MayuriK/videos" > <i class="fa fa-bell-o"></i> Other Video Demos <span class="badge badge-success pull-right m-l-10 m-b-10" style="font-size:20px;" >Click here to check</span></a>
+</li>
+<li class="list-group-item">
+<a style="font-size:20px;" href="#"> <i class="fa fa-comments-o"></i>Total Projects Developed by me till Date <span class="badge badge-warning pull-right r-activity m-l-10 m-b-10"  style="font-size:20px;" >103</span></a>
+</li>
+</ul>
+</section>
+                    </div>
+                  
+                </div>
+                
+               
+
+
+<?php include('./constant/layout/footer.php');?>
+<!--  Author Name: Mayuri K. 
+ for any PHP, Codeignitor or Laravel work contact me at mayuri.infospace@gmail.com  -->
+
