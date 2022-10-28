8b878e56e449e914c0c319b954a9ade653c02fa2
MBAJERWAKAERIC <erickmbanjerwaka@gmail.com>
2022-10-28 15:15:36 +0300

    Create author.php

---
+++ admin/author.php
@@ -0,0 +1,74 @@
+<?php include('../constant/layout/head.php');?>
+<?php include('../constant/layout/header.php');?>
+<?php include('../constant/layout/sidebar.php');
+?>
+ 
+
+  <!-- Page wrapper  -->
+        <div class="page-wrapper">
+            <!-- Bread crumb -->
+            <div class="row page-titles">
+                <div class="col-md-5 align-self-center">
+                    <h3 class="text-primary">About Original Author</h3> </div>
+                <div class="col-md-7 align-self-center">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+                        <li class="breadcrumb-item active">Original Author</li>
+                    </ol>
+                </div>
+            </div>
+            <!-- End Bread crumb -->
+            <!-- Container fluid  -->
+            <div class="container-fluid">
+                <!-- Start Page Content -->
+                
+                <!-- /# row -->
+                 <div class="card">
+                            <div class="card-body">
+                              
+                            
+                         
+                                                <div class="row">
+                    <div class="col-lg-8" style="    margin-left: 10%;">
+                        <section class="card">
+<div class="card-header user-header alt bg-dark">
+<div class="media">
+
+<div class="media-body">
+<h2 class="text-light display-6">Author Name : Mayuri K</h2>
+<p style="color: white">Freelancer - PHP, Codeignitor & Laravel</p>
+</div>
+</div>
+</div>
+<ul class="list-group list-group-flush">
+<li class="list-group-item">
+<a href="#" style="font-size:20px;" > <i class="fa fa-envelope-o"></i> Email <span class="badge badge-primary pull-right m-l-10 m-b-10" style="font-size:20px;" >mayuri.infospace@gmail.com</span></a>
+</li>
+<li class="list-group-item">
+<a style="font-size:20px;" href="https://mayurik.com" target="_blank" > <i class="fa fa-tasks"></i> My Previous Work <span class="badge badge-danger pull-right m-l-10 m-b-10" style="font-size:20px;" >www.mayurik.com</span></a>
+</li>
+<li class="list-group-item">
+<a style="font-size:20px;" href="https://www.youtube.com/c/MayuriK/videos" > <i class="fa fa-bell-o"></i> Other Video Demos <span class="badge badge-success pull-right m-l-10 m-b-10" style="font-size:20px;" >Click here to check</span></a>
+</li>
+<li class="list-group-item">
+<a style="font-size:20px;" href="#"> <i class="fa fa-comments-o"></i>Total Projects Developed by me till Date <span class="badge badge-warning pull-right r-activity m-l-10 m-b-10"  style="font-size:20px;" >105</span></a>
+</li>
+</ul>
+</section>
+<h2 style="color: red">Contact me for any Project Development at affordable cost.</h2>
+                    </div>
+                  
+                </div>
+                                </div>
+                            </div>
+                        </div>
+               
+                <!-- /# row -->
+
+                <!-- End PAge Content -->
+           
+
+<?php include('../constant/layout/footer.php');?>
+  <!--  This Project Developed by: Mayuri K. 
+ for any PHP, Codeignitor or Laravel work contact me through https://www.mayurik.com  -->
+
